d6d2fbb34e9c434ee0acfc833e6001bfb03a4026
Pankaj Kumar <pankajshakya12345@gmail.com>
2025-12-08 03:38:05 +0530

    fixced CI

--- terraform/guardduty.tf
+++ terraform/guardduty.tf
@@ -6,16 +6,18 @@ resource "aws_guardduty_detector" "main" {
   count = var.enable_guardduty ? 1 : 0
   enable = true
 
-  datasources {
-    s3_logs {
-      enable = true
-    }
-    kubernetes {
-      audit_logs {
-        enable = true
-      }
-    }
-  }
+}
+
+resource "aws_guardduty_detector_feature" "s3_logs" {
+  detector_id = aws_guardduty_detector.main.id
+  name        = "S3_DATA_EVENTS"
+  status      = "ENABLED"
+}
+
+resource "aws_guardduty_detector_feature" "eks_audit_logs" {
+  detector_id = aws_guardduty_detector.main.id
+  name        = "EKS_AUDIT_LOGS"
+  status      = "ENABLED"
 
   tags = {
     Name        = "${var.project_name}-guardduty-${var.environment}"
