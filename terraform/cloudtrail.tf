bf813eaf228500a352041c8f54ee9eb613fb01b0
Pankswork <panks.work08@gmail.com>
2025-12-09 12:37:41 +0000

    Fix CloudTrail & Enable Monitoring

--- terraform/cloudtrail.tf
+++ terraform/cloudtrail.tf
@@ -115,6 +115,11 @@ resource "aws_s3_bucket_policy" "cloudtrail_logs" {
         }
         Action   = "s3:GetBucketAcl"
         Resource = aws_s3_bucket.cloudtrail_logs[0].arn
+        Condition = {
+          StringEquals = {
+            "aws:SourceArn" = "arn:aws:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/${var.project_name}-cloudtrail-${var.environment}"
+          }
+        }
       },
       {
         Sid    = "AWSCloudTrailWrite"
@@ -123,10 +128,11 @@ resource "aws_s3_bucket_policy" "cloudtrail_logs" {
           Service = "cloudtrail.amazonaws.com"
         }
         Action   = "s3:PutObject"
-        Resource = "${aws_s3_bucket.cloudtrail_logs[0].arn}/*"
+        Resource = "${aws_s3_bucket.cloudtrail_logs[0].arn}/cloudtrail-logs/*"
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
+            "aws:SourceArn" = "arn:aws:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/${var.project_name}-cloudtrail-${var.environment}"
           }
         }
       }
