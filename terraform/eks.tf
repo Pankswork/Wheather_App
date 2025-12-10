4307b1bf000ce1be03927ec378aed682f7429cd6
Pankswork <panks.work08@gmail.com>
2025-12-10 09:53:23 +0000

    Fix Kubernetes Provider Authentication

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -401,7 +401,12 @@ data "aws_eks_cluster_auth" "main" {
 provider "kubernetes" {
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
+  }
 }
 
 # ============================================================================
