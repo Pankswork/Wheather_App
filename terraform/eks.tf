65783d8c1fc11e55970674efb9aff6effd4ace09
Pankswork <panks.work08@gmail.com>
2025-12-10 09:49:12 +0000

    Enable EKS API Authentication Mode

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -62,6 +62,11 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster.arn
   version  = var.eks_cluster_version
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   vpc_config {
     subnet_ids              = aws_subnet.private[*].id
     security_group_ids      = [aws_security_group.eks_cluster.id]
