2d367930513bb8b16a4746580497bc5b8b6da1de
Pankswork <panks.work08@gmail.com>
2025-12-10 07:48:43 +0000

    fixed eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -257,6 +257,8 @@ resource "aws_eks_addon" "vpc_cni" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "vpc-cni"
 
+  depends_on = [aws_eks_node_group.main]
+
   tags = {
     Name        = "${var.project_name}-vpc-cni-${var.environment}"
     Environment = var.environment
@@ -269,6 +271,8 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "coredns"
+
+  depends_on = [aws_eks_node_group.main]
 
   tags = {
     Name        = "${var.project_name}-coredns-${var.environment}"
@@ -283,6 +287,8 @@ resource "aws_eks_addon" "kube_proxy" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "kube-proxy"
 
+  depends_on = [aws_eks_node_group.main]
+
   tags = {
     Name        = "${var.project_name}-kube-proxy-${var.environment}"
     Environment = var.environment
@@ -295,6 +301,8 @@ resource "aws_eks_addon" "kube_proxy" {
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name = aws_eks_cluster.main.name
   addon_name   = "aws-ebs-csi-driver"
+
+  depends_on = [aws_eks_node_group.main]
 
   tags = {
     Name        = "${var.project_name}-ebs-csi-${var.environment}"
